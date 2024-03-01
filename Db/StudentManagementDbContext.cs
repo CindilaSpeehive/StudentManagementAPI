1dc641b35f3aa7da6606b349b65eb5d0a93b6e6e
jkjayakrishnan <jkjayakrishnanjk901@gmail.com>
2024-03-01 10:10:05 +0530

    dbcontext added

---
+++ Db/StudentManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementAPI.Models;
+
+namespace StudentManagementAPI.Db
+{
+    public class StudentManagementDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = "Server=localhost\\SQLEXPRESS;Database=StudentManagementDb;Trusted_Connection=True;Encrypt=false;TrustServerCertificate=true";
+            optionsBuilder.UseSqlServer(path);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
