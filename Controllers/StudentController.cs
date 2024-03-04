8e6d3d578272673daa9fba471eb9f57dfcd9e866
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2024-03-04 09:39:34 +0530

    style: code format

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -3,61 +3,53 @@ using StudentManagementAPI.Db;
 using StudentManagementAPI.Models;
 using StudentManagementAPI.Services;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace StudentManagementAPI.Controllers
+namespace StudentManagementAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StudentController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StudentController : ControllerBase
+    // GET: api/<StudentController>
+
+    private readonly IStudentService _studentService;
+    public StudentController()
     {
-        // GET: api/<StudentController>
+        _studentService = new StudentService(); //  remove new 
+    }
+    [HttpGet]
+    public List<Student> GetList()
+    {
+        return _studentService.GetAll();
+    }
 
-        private readonly IStudentService _studentService;
-        public StudentController()
-        {
-               
-  
-            _studentService = new StudentService(); //  remove new 
-        }
-        [HttpGet]
-        public List<Student> GetList()
-        {
-            return _studentService.GetAll();
+    // GET api/<StudentController>/5
+    [HttpGet("{id}")]
+    public Student Get(int id)
+    {
+        Student studentDetail=_studentService.Get(id);
+        return  studentDetail;
+    }
 
-        }
+    // POST api/<StudentController>
+    [HttpPost]
+    public void Post( Student value)
+    {
+        _studentService.Insert(value);
 
-        // GET api/<StudentController>/5
-        [HttpGet("{id}")]
-        public Student Get(int id)
-        {
-            Student studentDetail=_studentService.Get(id);
-            return  studentDetail;
-        }
+    }
 
-        // POST api/<StudentController>
-        [HttpPost]
-        public void Post( Student value)
-        {
-            _studentService.Insert(value);
+    // PUT api/<StudentController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Student updateValue)
+    {
+       _studentService.Update(updateValue, id);
+    }
 
-        }
-
-        // PUT api/<StudentController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Student updateValue)
-        {
-           _studentService.Update(updateValue, id);
-        }
-
-        // DELETE api/<StudentController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            _studentService.Delete(id);
-        }
-
-        // change1
-        // chnage2
+    // DELETE api/<StudentController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        _studentService.Delete(id);
     }
 }
